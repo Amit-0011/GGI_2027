63f5dd48148eeedd70c4be6431344da54df8a43e
Amit Kumar <kamit363693@gmail.com>
2025-10-30 22:08:54 +0530

    videos completed

---
+++ Assignment_01/main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+ 
+int main()
+{
+    int x = 5;
+    int* p = &x;
+    *p = 6;
+    int** q = &p;
+    int*** r = &q;
+    printf("%d\n",*p); // 6
+    printf("%d\n",*q); // 225
+    printf("%d\n",*(*q)); // 6
+    printf("%d\n",*(*r)); // 225
+    printf("%d\n",*(*(*r))); // 6
+    ***r = 10;
+    printf("x = %d\n",x);
+    **q = *p +2;
+    printf("x = %d\n",x);
+
+}
